b04baf80c0459804020a85e2a3431eedcc557a9b
lauritzpepke <s191179@student.dtu.dk>
2022-03-15 14:31:09 +0100

    new components

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Navbar from '../components/Navbar/Navbar.js';
 import Topbar from '../components/Topbar/Topbar.js';
 import Book from '../components/Book.js';
+import { ItemImage } from '../components/ProductPageComponents/ItemImage.js';
+import ItemDescription from '../components/ProductPageComponents/ItemDescription.js';
 
 function ProductPage() {
     return (
         <>
         <Topbar></Topbar>
         <Navbar></Navbar>
-        <div>
+        <div classname='rowA'>
+            <ItemImage/>
+        </div>
+        <div className='rowB'>
             <Book/>
+            <ItemDescription/>
         </div>
         </>
     )
---
+++ src/components/ProductPageComponents/ItemImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../logo.svg'
+
+export const ItemImage = () => {
+  return (
+    <img src={logo} className="itemImage" width={350} height={350}></img>
+  )
+}
+
+export default ItemImage
+
