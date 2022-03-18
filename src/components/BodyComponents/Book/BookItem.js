2bb6307c9f1718cb4e7e049d5c1c45d6c5b96ab3
Kristoffer <kristoffer@kritix.dk>
2022-03-18 10:57:39 +0100

    book stuff

--- src/components/BodyComponents/Book/BookItem.js
+++ src/components/BodyComponents/Book/BookItem.js
@@ -15,4 +15,36 @@ export const BookItem = [
         city: 'Horsens',
         country: 'Denmark'
     },
+    {
+        title: 'Parallel Programming and you',
+        description: 'Programming in a parallel world',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41gr4XJnUjL._SX321_BO1,204,203,200_.jpg',
+        price: '250,00 kr',
+        city: 'Horsens',
+        country: 'Denmark'
+    },
+    {
+        title: 'Parallel Programming and you',
+        description: 'Programming in a parallel world',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41gr4XJnUjL._SX321_BO1,204,203,200_.jpg',
+        price: '250,00 kr',
+        city: 'Horsens',
+        country: 'Denmark'
+    },
+    {
+        title: 'Parallel Programming and you',
+        description: 'Programming in a parallel world',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41gr4XJnUjL._SX321_BO1,204,203,200_.jpg',
+        price: '250,00 kr',
+        city: 'Horsens',
+        country: 'Denmark'
+    },
+    {
+        title: 'Parallel Programming and you',
+        description: 'Programming in a parallel world',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41gr4XJnUjL._SX321_BO1,204,203,200_.jpg',
+        price: '250,00 kr',
+        city: 'Horsens',
+        country: 'Denmark'
+    },
 ]
