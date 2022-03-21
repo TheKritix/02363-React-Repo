a23b90da16185247ea73bab93a71a4a9d2b4c8ef
Kristoffer <kristoffer@kritix.dk>
2022-03-21 22:19:09 +0100

    Grid working ish

--- src/components/BodyComponents/Book/BookOverview.js
+++ src/components/BodyComponents/Book/BookOverview.js
@@ -3,6 +3,7 @@ import Book from './Book';
 import { BookItem } from './BookItem.js';
 import './BookOverview.css'
 import { useState } from 'react'
+import { Grid } from '@mui/material';
 
 export const BookOverview = () => {
 
@@ -11,24 +12,30 @@ export const BookOverview = () => {
 
     return (
         <div>
-            <div className="BookItem">
-                <div className="invisBox">
-                    <div className="SideMenu">
-                        <p id="search">Search: </p>
-                        <input value={search} onChange={(e) => setSearch(e.target.value)} />
-                        <p id="title">Title: </p>
-                        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-                    </div>
-                </div>
+            <div className="SideMenu">
+                <p id="search">Search: </p>
+                <input value={search} onChange={(e) => setSearch(e.target.value)} />
+                <p id="title">Title: </p>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div className = "gridBox">
+            <Grid
+                margin={2}
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+            >
                 {BookItem.map((item, index) => {
                     return (
-                        <div className="ItemBox">
-                            <div>
+                        <div className='indiGrid'>
+                            <Grid container item xs={12} sm={3} md={12} key={index}>
                                 <Book className="Book" item={item} />
-                            </div>
+                            </Grid>
                         </div>
                     )
                 })}
+            </Grid>
             </div>
         </div>
     )
