ad2306e5e27e464b263a8904adb91bc427b21aab
lauritzpepke <s191179@student.dtu.dk>
2022-04-04 15:13:47 +0200

    fix

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Navbar from "../components/Navbar/Navbar.js";
-import Topbar from "../components/Topbar/Topbar.js";
-import { ItemImage } from "../components/ProductPageComponents/ItemImage.js";
-import ItemDescription from "../components/ProductPageComponents/ItemDescription.js";
-
-function ProductPage() {
-  return (
-    <>
-      <Topbar></Topbar>
-      <Navbar></Navbar>
-      <div classname="rowA">
-        <ItemImage />
-      </div>
-      <div className="rowB">
-        <ItemDescription />
-      </div>
-    </>
-  );
-}
-export default ProductPage;
-=======
 import React from 'react';
 import { useState } from 'react'
 import Navbar from '../components/Navbar/Navbar.js';
@@ -85,4 +62,3 @@ function ProductPage() {
     )
   }
   export default ProductPage;
->>>>>>> origin/dev_productpage
--- src/components/ProductPageComponents/ItemImage.js
+++ src/components/ProductPageComponents/ItemImage.js
@@ -2,10 +2,7 @@ import React from "react";
 import logo from "../../logo.svg";
 
 export const ItemImage = () => {
-<<<<<<< HEAD
-  return <img src={logo} className="itemImage" width={350} height={350}></img>;
-};
-=======
+
   return (
     <img 
     src='https://m.media-amazon.com/images/I/51eS3MxQ1YL.jpg'
@@ -16,6 +13,3 @@ export const ItemImage = () => {
 }
 
 export default ItemImage
->>>>>>> origin/dev_productpage
-
-export default ItemImage;
