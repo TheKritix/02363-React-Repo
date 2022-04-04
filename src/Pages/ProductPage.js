cbd1ac4fe1f2494365f9c545dcfe261f8fec9598
lauritzpepke <s191179@student.dtu.dk>
2022-04-04 10:28:19 +0200

    Button function - partly working

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useState } from 'react'
 import Navbar from '../components/Navbar/Navbar.js';
 import Topbar from '../components/Topbar/Topbar.js';
 import { ItemImage } from '../components/ProductPageComponents/ItemImage.js';
+import ItemTitle  from '../components/ProductPageComponents/ItemTitle.js';
 import ItemDescription from '../components/ProductPageComponents/ItemDescription.js';
 import Card from "@material-ui/core/Card";
 import { CardContent } from '@mui/material';
 import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import IconButton from "@material-ui/core/IconButton";
+import { Favorite } from '@mui/icons-material';
 
 function ProductPage() {
+
+    const [isFavClicked, setFavClicked] = useState(false);
+
     return (
         <>
         <Topbar></Topbar>
@@ -23,12 +31,20 @@ function ProductPage() {
                     margin: 10,             
                 }}>
                     <CardContent>
-                        <ArrowBackRoundedIcon/>
-                        <FavoriteBorder style={{
-                            float: 'right',
-                            }}/>
+                        <IconButton onClick={event => window.location.href='/buy'}>
+                            <ArrowBackRoundedIcon/>
+                        </IconButton>
+
+                        {isFavClicked === false
+                        ? <IconButton onClick={() => setFavClicked === true} style={{float: 'right'}}>
+                            <FavoriteBorder/>
+                        </IconButton>
+                        : <IconButton onClick={() => setFavClicked === false} style={{float: 'right'}}>
+                            <FavoriteIcon/>
+                        </IconButton>
+                        }
                         <div style={{display: 'flex', justifyContent: 'Center'}}>
-                            <ItemImage />
+                            <ItemImage/>
                         </div>
                         <h3>Book with text in it</h3> 
                         <h3 id='price'>999 kr</h3>
@@ -38,6 +54,7 @@ function ProductPage() {
                 
             </div>
             <div className='rowC'>
+                <ItemTitle/>
                 <ItemDescription/>
             </div>
         </div>
