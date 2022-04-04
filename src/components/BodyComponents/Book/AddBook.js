1ca6aa8c6043575531728b7d6e9227c1c8531146
Arcspider <arcspider45@gmail.com>
2022-04-04 16:13:46 +0200

    minor update

--- src/components/BodyComponents/Book/AddBook.js
+++ src/components/BodyComponents/Book/AddBook.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 //import { BookItem } from './BookItem.js';
 import Navbar from "../../Navbar/Navbar";
 import Topbar from "../../Topbar/Topbar";
+
 
 export const AddBook = () => {
   const [books, setBooks] = useState([
@@ -32,7 +33,7 @@ export const AddBook = () => {
       Lang: "English",
       Topic: "Physics",
       Image: null,
-    },
+    }
   ]);
 
   const handleChange = (e) => {
@@ -40,7 +41,6 @@ export const AddBook = () => {
       ...books,
       [e.target.name]: e.target.value,
     });
-    
   };
 
   const addBook = (e) => {
@@ -48,7 +48,7 @@ export const AddBook = () => {
     e.preventDefault();
     const bookObject = {
       Book_Id: 2,
-      Title: "Woop",
+      Title: books.Title,
       Author: "test",
       Price: 123,
       Publisher: "Test",
@@ -58,9 +58,9 @@ export const AddBook = () => {
       Course: "Test",
       Lang: "Test",
       Topic: "Test",
-      image: null,
+      Image: null,
     };
-    setBooks([books, bookObject]);
+    setBooks(...books,bookObject);
     //setBooks(...books.concat(bookObject))
   };
   const handleSubmit = (e) => {
@@ -81,17 +81,19 @@ export const AddBook = () => {
       <Navbar></Navbar>
       <h1>Books</h1>
       <h2>Add book:</h2>
-      <form className="AddBook" onSubmit={addBook}>
+      <form className="AddBook">
         <div>
           <label id="bookTitle"> Title: </label>
           <input
             name="Title"
             type="text"
-            value={books.value}
+            value={books.name}
             onChange={handleChange}
           />
 
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={addBook}>
+            Submit
+          </button>
         </div>
       </form>
       <h2>Book list:</h2>
