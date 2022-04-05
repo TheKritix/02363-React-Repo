22664b8e2a6a0421d00d354b26203ef2c8c11183
lauritzpepke <s191179@student.dtu.dk>
2022-04-05 15:15:59 +0200

    book

--- src/components/BodyComponents/Book/Book.js
+++ src/components/BodyComponents/Book/Book.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Book.css";
+import ButtonBase from '@mui/material/ButtonBase';
 
 export const Book = ({ item }) => {
   return (
+    <ButtonBase onClick={() => window.location.href='/productpage'}>
     <div className="ItemBox">
       <img id="bookImage" src={item.imageUrl} alt="Logo" />
       <p id="bookTitle">{item.title}</p>
       <p id="price">{item.price}</p>
       <p id="city">{item.city}</p>
     </div>
+    </ButtonBase>
   );
 };
 export default Book;
