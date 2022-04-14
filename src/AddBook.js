7264920604d07b9b87ed00335032386287ab816b
Kristoffer <kristoffer@kritix.dk>
2022-04-14 15:12:17 +0200

    Added some changes

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,164 +1,177 @@
-import './App.css';
+import "./App.css";
 
-import React, { useState } from 'react'
-import Book from "./components/book"
+import React, { useState } from "react";
+import Book from "./components/book";
 
 const AddBook = () => {
+  const [books, setBooks] = useState([
+    {
+      title: "Physics 101",
+      publisher: "Waterloo",
+      price: 200,
+      university: "Starbucks",
+      cond: "Great",
+      course: "10020",
+      lang: "English",
+      topic: "Physics",
+      image: "",
+      description: "A great book for physics",
+    },
+  ]);
 
-    const [books, setBooks] = useState([{
-        id: 1,
-        title: 'Physics 101',
-        publisher: 'Waterloo',
-        price: '200 DKK',
-        University_name: 'Starbucks',
-        Condition: 'Great',
-        Course: '10020',
-        Language: 'English',
-        Topic: 'Physics',
-        Image: '',
-        description: "A great book for physics"
-    }
-    ])
+  const handleChange = (e) => {
+    setBooks({
+      ...books,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleChange = e => {
-        setBooks({
-            ...books,
-            [e.target.name]: e.target.value,
-        })
-    }
-
-    const addBook = (e) => {
-        e.preventDefault()
-        const bookObject = {
-        id: 2,
-        title: books.title,
-        publisher: books.publisher,
-        price: books.price,
-        University_name: books.University_name,
-        Condition: books.Condition,
-        Course: books.Course,
-        Language: books.Language,
-        Topic: books.Topic,
-        Image: books.Image,
-        description: books.description
-        }
-        setBooks(books.concat(bookObject))
-        setBooks({
-            ...books,
-            [e.target.name]: "",
-        })
-    }
+  const addBook = (e) => {
+    e.preventDefault();
+    const bookObject = {
+      title: books.title,
+      publisher: books.publisher,
+      price: books.price,
+      university: books.university,
+      cond: books.cond,
+      course: books.course,
+      lang: books.lang,
+      topic: books.topic,
+      image: books.image,
+      description: books.description,
+    };
+    console.log(bookObject)
+    fetch('http://stoodle.bhsi.xyz:3000/api/books',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: bookObject
+    })
+    // setBooks(bookObject.join("&"));
+    // console.log(books)
+    // setBooks({
+    //   ...books,
+    //   [e.target.name]: "",
+    // });
+  };
 
 
-    return (
-        <div>
-            <h1>Books</h1>
-            <h2>Add books:</h2>
-            <form>
-                <label>
-                    Book Title:{" "}
-                    <input
-                        type="text"
-                        name="title"
-                        value={books.title}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Publisher: {" "}
-                    <input
-                        type="text"
-                        name="publisher"
-                        value={books.publisher}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Price:{" "}
-                    <input
-                        type="number"
-                        name="price"
-                        value={books.price}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    University:{" "}
-                    <input
-                        type="text"
-                        name="uniName"
-                        value={books.University_name}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Condition:{" "}
-                    <input
-                        type="text"
-                        name="condition"
-                        value={books.Condition}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Course:{" "}
-                    <input
-                        type="number"
-                        name="course"
-                        value={books.Course}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Language:{" "}
-                    <input
-                        type="text"
-                        name="language"
-                        value={books.Language}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Topic:{" "}
-                    <input
-                        type="text"
-                        name="topic"
-                        value={books.Topic}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Image:{" "}
-                    <input
-                        type="text"
-                        name="Image"
-                        value={books.Image}
-                        onChange={handleChange}
-                    />
-                </label> <br/>
-                <label>
-                    Description:{" "}
-                    <input
-                        type="text"
-                        name="desc"
-                        value={books.description}
-                        onChange={handleChange}
-                    />
-                </label>
-            </form>
-            <h2>Book list:</h2>
-            
-            <div>
-                <button type="submit" onClick={addBook} >Submit</button>
-            </div>
-        </div>
-    )
-}
-/*
-{books.map((item) => (
-                <>
-                    <p key={item.id}> <b>Title:</b> {item.title}, <b>Topic:</b> {item.Topic} </p>
-                </>
-            ))}
-*/
-export default AddBook
+
+  return (
+    <div>
+      <h1>Books</h1>
+      <h2>Add books:</h2>
+      <form>
+        <label>
+          Book Title:{" "}
+          <input
+            type="text"
+            name="title"
+            value={books.title}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Publisher:{" "}
+          <input
+            type="text"
+            name="publisher"
+            value={books.publisher}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Price:{}
+          <input
+            type="number"
+            name="price"
+            value={books.price}
+            onChange={handleChange}
+          />
+        </label>{}
+        <br />
+        <label>
+          University:{" "}
+          <input
+            type="text"
+            name="uniName"
+            value={books.university}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          cond:{" "}
+          <input
+            type="text"
+            name="condition"
+            value={books.cond}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          course:{" "}
+          <input
+            type="number"
+            name="course"
+            value={books.course}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          lang:{" "}
+          <input
+            type="text"
+            name="language"
+            value={books.lang}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          topic:{" "}
+          <input
+            type="text"
+            name="topic"
+            value={books.topic}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          image:{" "}
+          <input
+            type="text"
+            name="image"
+            value={books.image}
+            onChange={handleChange}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Description:{" "}
+          <input
+            type="text"
+            name="desc"
+            value={books.description}
+            onChange={handleChange}
+          />
+        </label>
+      </form>
+      <h2>Book list:</h2>
+
+      <div>
+        <button type="submit" onClick={addBook}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddBook;
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './HomePage.css';
 import Navbar from './components/Navbar/Navbar';
 import Topbar from './components/Topbar/Topbar';
+import AddBook from './AddBook.js'
 
 function HomePage() {
     return (
         <>
         <Topbar></Topbar>
         <Navbar></Navbar>
+        <AddBook></AddBook>
         </>
     )
   }
