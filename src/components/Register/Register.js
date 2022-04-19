f6257abb9df244d7ca1d7015f6262ce2528e5b58
Camilla <s205360@student.dtu.dk>
2022-04-19 16:17:35 +0200

    new function added for registration

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,75 @@
 import React, { Component} from 'react';
 import './Register.css';
+import { useState } from 'react';
 
-class Register extends Component {
-    render() {
+export const Register = () => {
+const [register, setRegister] = useState([
+    {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+    
+    },
+])
+    
+
+    const handleRegistration = (e) => {
+        setRegister({
+            ...register,
+            [e.target.name]: e.target.value,
+
+        });
+        
+    };
+
+    const submitRegistration = (e) => {
+        e.preventDefault();
+        const registrationObject = {
+            firstname: register.firstname,
+            lastname: register.lastname,
+            email: register.email,
+            password: register.password
+        };
+        console.log(registrationObject);
+        if(!(registrationObject.email === "" && registrationObject.password === "")){
+            fetch("" ,{
+                method: "POST",
+                headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+            body: new URLSearchParams({ 
+                'firstname': `${register.firstname}`,
+                'lastname': `${register.lastname}`,
+                'email': `${register.email}`,
+                'password': `${register.password}`
+
+            })
+        }).then(() => {
+            console.log("New user added")
+        })
+
+        } 
+    }
+
+
+
         return (
             <div className="registerInput">
                 <h1 id="headlineRegister">Register</h1>
                 <p id="description">Please fill in this form to create an account.</p>
-                <form> 
-                    <input type="firstname" name="firstname" placeholder='First name' required></input>
+                <form onSubmit={submitRegistration}> 
+                    <input type="firstname" name="firstname" placeholder='First name' value = {register.firstname} onChange={handleRegistration} required
+                   ></input>
                     <br/>
-                    <input type="lastname" name="lastname" placeholder='Last name' required/>
+                    <input type="lastname" name="lastname" placeholder='Last name' value = {register.lastname} onChange={handleRegistration} required
+                    />
                     <br/>
-                    <input type="email" name="email" placeholder='Email' required/>
+                    <input type="email" name="email" placeholder='Email' value = {register.email} onChange={handleRegistration} required 
+                    />
                     <br/>
-                    <input type="password" name="password" placeholder='Password'  minlength="8" required/>
+                    <input type="password" name="password" placeholder='Password'  minlength="8" value = {register.password} onChange={handleRegistration} required
+                    />
                     <br/>
                     <input type="submit" value="Register"/>
                 </form>
@@ -22,6 +77,10 @@ class Register extends Component {
             </div>
         )
     }
-}
+
+
+
+
+
 
 export default Register;
