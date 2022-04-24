71f36b0b1805d3695b44aed84a3cdeab4fd55a3c
Kristoffer <kristoffer@kritix.dk>
2022-04-24 17:41:35 +0200

    qwe

---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Login  from "../components/Login/Login";
+import useToken from '../components/Login/useToken';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProfilePage from '../Pages/ProfilePage';
+
+function LoginPage() {
+
+    const {token, setToken} = useToken();
+    if(!token) {
+        return <Login setToken={setToken}/> 
+      } 
+
+    return 
+  }
+  export default LoginPage;
