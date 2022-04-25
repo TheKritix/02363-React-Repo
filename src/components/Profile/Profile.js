ed275ec13715924ab5d2da9176b12bf9d6ef1e2e
Thamara <s205337@student.dtu.dk>
2022-04-25 21:22:32 +0200

    Saving login datetails underprofil

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./Profile.css";
 import profileImg from "./profileIcon.png";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import StarOutlineIcon from "@mui/icons-material/StarOutline";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
-class Profile extends Component {
-  render() {
+
+export  const Profile = () => {
+
+  const userId = window.sessionStorage.getItem('userId');
+  console.log(userId);
+
+  const [userInfo, setUserInfo] = useState([]);
+  
+  const retriveUserInfo = () => {
+    fetch(`http://localhost:3001/api/userinfo/${userId}`, {
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+          }
+    })
+    .then ((response) => {
+      console.log(response)
+      return response.json();
+    })
+    .then((JSON) => {
+      console.log(JSON);
+      setUserInfo(JSON);
+    });
+  }
+
+  useEffect(() => {
+    retriveUserInfo()
+  }, [])
+
     return (
       <div className="profileLayout">
-        <div className="infoContainer">
           <div className="divIcon">
-            <CameraAltIcon
-              className="cameraIcon"
-              style={{ fontSize: 30, left: 200 }}
-            ></CameraAltIcon>
             <img
               src={profileImg}
               alt="profileImg"
@@ -22,15 +44,22 @@ class Profile extends Component {
               height={180}
             ></img>
           </div>
-
           <div className="profileInfo">
-            <span>Name </span>
+          {userInfo.map((item, index)=> { 
+            return (
+            <li key={index}>
+                <h5>{item.Firstname}</h5>
+                <h5>{item.Lastname}</h5>
+                <h5>{item.EMAIL}</h5>
+            </li>
+            );
+          })}
+            <span>  </span>
             <br />
-            <span>Email</span>
+            <span></span>
             <br />
-            <span>Phone number</span>
+            <span></span>
           </div>
-        </div>
 
         <div className="container">
           <div className="buttons">
@@ -52,6 +81,10 @@ class Profile extends Component {
       </div>
     );
   }
-}
 
 export default Profile;
+
+/* <CameraAltIcon
+              className="cameraIcon"
+              style={{ fontSize: 30, left: 200 }}
+            ></CameraAltIcon>*/
