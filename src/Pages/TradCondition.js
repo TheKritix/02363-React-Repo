5cb8acfd7ea0b9275b96283206ff7c79c255b1cb
Kristoffer <kristoffer@kritix.dk>
2022-04-27 11:39:09 +0200

    Added ToS and TradeConditions

---
+++ src/Pages/TradCondition.js
@@ -0,0 +1,15 @@
+import Profile from "../components/Profile/Profile";
+import Menu from '../components/Profile/ProfilMenu/ProfileMenu'
+import Tradcon from "../components/TradCon/TradCon"
+
+function Tradcondition() {
+    return(
+        <>
+        <Profile></Profile>
+        <Menu></Menu>
+        <Tradcon></Tradcon>
+        </>
+    )
+}
+
+export default Tradcondition;
---
+++ src/components/TradCon/TradCon.js
@@ -0,0 +1,193 @@
+import React, {Component} from "react";
+import Menu from '../Profile/ProfilMenu/ProfileMenuList';
+import "./TradCon.css"
+
+class Tradcon extends Component {
+render() {
+return (
+<>
+
+<div className="tra">
+
+1. General provisions<br></br>
+1.1. These General Trading Terms and Conditions form an essential and necessary part of every offer and<br></br>
+every agreement concluded with the Contractor. By acceptance of these terms and conditions, within<br></br>
+the meanning of section 263 of the Commercial Code, Section III, both parties exclude statutory<br></br>
+regulations of the Commercial Code, Section III, which are inconsistent with them.<br></br>
+1.2. The Contract is not bound by any trading terms and conditions mentioned in the Buyer's written<br></br>
+documents. Signing an order and its sending back is deemed to be the order confirmation only, not<br></br>
+the acceptance of the Buyer's trading terms and conditions.<br></br><br></br>
+2. Modification of terms and conditions<br></br>
+2.1. These General Trading Terms and Conditions may only be modified by written amendments, signed<br></br>
+by a person authorized by the Contractor.<br></br><br></br>
+3. Prices<br></br>
+3.1. For deliveries of goods from the Contractor's warehouse in the Czech Republic, prices of goods shall<br></br>
+be quoted in Czech Crowns or in Euros, depending on the specific contract.<br></br>
+3.2. Delivery shall be billed by the Contractor as of the day that goods are delivered.<br></br><br></br>
+4. Terms of payment<br></br>
+4.1. All payments shall be payable within 14 days of the day that the respective invoice is issued, unless<br></br>
+otherwise agreed. The day of payment is regarded to be the day the sum is credited to the<br></br>
+Contractor's account and is free for disposal, unless otherwise agreed.<br></br>
+4.2. Unless otherwise agreed, all payments in currency of CZK shall be remitted into the Contractor's<br></br>
+bank account No.17626413/0300, at Československá obchodní banka,a.s., and all payments in<br></br>
+currency of EUR shall be remitted into the Contractor's bank account No.06429280/0300, at<br></br>
+Československá obchodní banka,a.s. Payments of any Contractor's claims to individual Contractor's<br></br>
+employees with the legal effect of debt repayment shall only be acceptable for cash payments and if<br></br>
+the Contractor's employee proves the authorization to collect, issued by the Contractor.<br></br>
+4.3. Should the payment term not be kept, the Buyer shall be in default and any other existing claims<br></br>
+against the Buyer as of that day shall become payable with immediate effect. The Contractor's claims<br></br>
+against the Buyer shall also become payable with immediate effect if a motion for the institution of<br></br>
+insolvency proceedings with respect to Buyer's assets is filed, or if such proceedings are commenced,<br></br>
+or if the Buyer actually suspends payments. In such cases, the Contractor shall also be entitled to<br></br>
+cancel current deliveries, or to request immediate payment of the purchase price in cash, or to request<br></br>
+an advance payment. The Contractor's right to compensation for damages on account of failure to<br></br>
+fulfil shall be retained by the Contractor.<br></br>
+4.4. If the Buyer does not pay the purchase price within the term of payment, the parties agree a<br></br>
+contractual penalty, amounting to 0.05% of the due amount per every started day of the Buyer's<br></br>
+delay in payment, but not less than CZK 7,000. By payment of the contractual penalty, the<br></br>
+2
+Contractor's right to compensation for damages shall not be affected; the Contractor shall be entitled<br></br>
+to claim for damages even in an amount exceeding the paid contractual penalty. Costs of legal<br></br>
+assistance related to debt recovery shall be regarded as ancillary rights of the debt and shall be billed<br></br>
+to the Buyer separately. In individual cases the Contractor may waive its claim for payment of the<br></br>
+contractual penalty.<br></br>
+4.5. The Buyer shall not be entitled to refuse or to defer any payment on account of a complaint about<br></br>
+defects. In the case of a complaint about visual defects, the payment shall be solved by mutual<br></br>
+agreement of both parties.<br></br>
+4.6. The Contractor shall be entitled to suspend delivery of ordered products till the Buyer fulfils all its<br></br>
+obligations against the Contractor existing as of the agreed date of delivery.<br></br>
+4.7. The prices of goods as quoted by the Contractor are based, among other things, on the prices of raw<br></br>
+materials, employees' wages, prices of energy, costs required for replacement of missing raw<br></br>
+materials by other raw materials and exchange rate fluctuations. The Contractor predominantly<br></br>
+reserves the right to change prices of goods accordingly if the prices or values of the items as stated<br></br>
+above change. However, such a change shall not affect any delivery of goods executed on the basis<br></br>
+of an order confirmed by the Contractor before the prices of goods were changed.<br></br>
+4.8. The Buyer undertakes to indicate the Contractor's bank account number, constant symbol, variable<br></br>
+symbol, remitted amount and due date in every payment order given to its bank.<br></br><br></br>
+5. Delivery<br></br>
+5.1. Unless otherwise agreed, the Seller shall choose the carrier and shall pay costs connected with the<br></br>
+transport of goods to the place of destination; goods shall always be delivered and shipped at the risk<br></br>
+of the Buyer (CPT, Incoterms 2000). The method of shipment shall be determined after agreement<br></br>
+with the Buyer.<br></br>
+5.2. The transport shall only be insured if it is ordered by the Buyer in writing, at the Buyer's expense.<br></br>
+5.3. If an order for goods was not cancelled properly and the goods cannot be delivered to the Buyer for a<br></br>
+reason on the part of the Buyer, and the Contractor cannot use the goods for further sale of<br></br>
+processing, the goods shall be stored at the risk of the Buyer. The costs for storage of such goods<br></br>
+shall be 4% of the purchase price of the goods per every started month, commencing on the first day<br></br>
+of such storage. Should the Buyer not take goods over within one month of the agreed takeover date,<br></br>
+the Contractor shall be entitled to terminate the contract, and the Buyer shall pay the Contractor the<br></br>
+costs for storage or alternative location of the goods (if paid by the Contractor) and a contractual<br></br>
+penalty, amounting to 20% of the price of the stored goods. By agreement or payment of the<br></br>
+contractual penalty, the Contractor's right to compensation for damages shall not be affected.<br></br>
+5.4. If goods are delivered on pallets, disposable pallets or euro pallets shall be used, unless otherwise<br></br>
+agreed. Unless otherwise agreed, the cost of pallets shall be included in the price of delivered goods,<br></br>
+and the pallets shall not be returnable.<br></br><br></br>
+6.Delivery terms<br></br>
+6.1. Goods may be collected from the warehouse within 7 – 10 working days, or within 14 working days<br></br>
+of written or phone order confirmation if delivered goods are cardboard tubes determined for further<br></br>
+processing. If the Buyer, however, confirmed the other delivery condition of EXW, he must always<br></br>
+ask in advance whether the required goods are in stock.<br></br><br></br>
+7. Joint provisions<br></br>
+7.1. The Contractor shall not be responsible for a breach of any of its obligations under the contractual<br></br>
+relationship arisen in consequence of unforeseeable obstacles, among others in manufacture, during<br></br>
+transport, in the case of supply problems at the company of the Contractor or its suppliers, or due to<br></br>
+force majeure. For the purposes of contractual relations between the Contractor and the Buyer, force<br></br>
+majeure is primarily deemed to be the breakdown of the Contractor's machines, shortage of driving<br></br>
+force or energy, strike, shutdown, fire, flood and any circumstance beyond the control of the<br></br>
+Contractor due to which the Contractor cannot fulfil its obligations. If the Contractor cannot fulfil its
+3<br></br><br></br>
+obligations due to force majeure temporarily, its obligations under the contract shall be suspended till<br></br>
+the effects of force majeure terminate. If the Contractor cannot fulfil its obligations due to force<br></br>
+majeure in full for unlimited period of time, or for more than one month, the Contractor shall be<br></br>
+entitled to terminate this Contract. Should the facts as provided in Art. 7.1 of the General Trading<br></br>
+Terms and Conditions above occur, the Buyer shall not be entitled to compensation for damages. The<br></br>
+Contractor undertakes to notify the Buyer of force majeure and its consequences without undue<br></br>
+delay.<br></br>
+7.2. The Contractor shall not compensate the Buyer for damages if delivery terms are exceeded.<br></br><br></br>
+8. Defects of goods<br></br>
+8.1. The Buyer shall check delivered goods carefully as soon as possible, and any complaints referring to<br></br>
+delivered goods must be filed with the Contractor by e-mail or by registered mail within 3 days of<br></br>
+the takeover of goods according to the delivery note. If a complaint is sent by e-mail, consequently a<br></br>
+written notice must be sent by registered mail within seven days of the goods takeover according to<br></br>
+the delivery note. A complaint should be attached with photo documentation of the established<br></br>
+defects of goods. After expiry of the said period, the goods shall be considered to be free of defects,<br></br>
+with the exception of possible hidden defects. Damages caused by transport must be notified<br></br>
+immediately in a suitable manner and documented in a record, made in the presence of the carrier<br></br>
+upon the takeover of goods from the carrier.<br></br>
+8.2. Complaints about hidden defects of goods must be filed with the Contractor by the e-mails and then<br></br>
+also by the registered mail immediately after their establishment.<br></br>
+8.3. All guarantees and rights to compensation for damage shall be excluded if goods were stolen or used,<br></br>
+as well as if goods were changed without the knowledge and approval of the Contractor. The<br></br>
+Contractor's liability for defects shall be excluded if storage conditions as recommended by the<br></br>
+Contractor were not kept.<br></br>
+8.4. The justification of a complaint filed in due time shall be established by the Contractor's Inspection<br></br>
+Department. The Buyer must submit the goods under complaint to the Seller. If the Contractor does<br></br>
+not accept a complaint and in spite of that the Buyer insists on the complaint, the Contractor shall<br></br>
+submit the goods under complaint to an authorized test laboratory for assessment. The result of the<br></br>
+assessment by the authorized test laboratory shall be final, and the costs incurred in connection with<br></br>
+the examination shall be paid by the losing party against which the assessment result was issued.<br></br>
+8.5. If a complaint is admitted as justified, the Contractor, after agreement with the Buyer, must either<br></br>
+provide replacement or credit the corresponding amount for the benefit of the Buyer.<br></br>
+8.6. Any returned goods shall only be accepted after previous written agreement and in the original<br></br>
+packing. Should any doubt about the price arise, the price agreed for delivery of the goods to the<br></br>
+Buyer shall apply.<br></br><br></br>
+9. Reservation of title<br></br>
+9.1. The title to all goods delivered by the Contractor shall be retained by the Contractor till the full<br></br>
+payment of the purchase price, including all fees, has been made. If the Contractor has several<br></br>
+overdue claims against the Buyer, the title to goods shall pass to the Buyer only after the payment of<br></br>
+all the claims, including their ancillary rights.<br></br>
+9.2. The Buyer must use any delivered goods that are still owned by the Contractor carefully and must<br></br>
+store them accordingly. The Buyer undertakes to follow all of the Contractor's instructions as given<br></br>
+to the Buyer by the Contractor in order to protect the goods or its title.<br></br>
+9.3. Any pledge or other encumbrance of products owned by the Contractor is not acceptable. The<br></br>
+Contractor must be notified of any act of a third person against the Contractor's goods by reason of<br></br>
+intervention.<br></br>
+9.4. By agreement of the reservation of the title, the time that the liability for damage to goods passes to<br></br>
+the Buyer under law or under these General Trading Terms and Conditions shall not be affected.<br></br>
+9.5. The Contractor shall be entitled to request immediate return of any goods delivered if the Buyer did<br></br>
+not pay the price of goods duly and on time, or if the Buyer actually suspended its payments.<br></br>
+4<br></br><br></br>
+9.6. Unless otherwise agreed between the parties, if the Contractor takes any goods back, it does not<br></br>
+mean that the Contractor terminates the contract. If sold goods, still owned by the Contractor, are<br></br>
+taken back, the Contractor shall retain the right to claim for damages on account of failure to fulfil a<br></br>
+contractual obligation.<br></br>
+9.7. Till the reservation of the title expires, the Buyer shall be considered as the steward of the goods sold<br></br>
+with the reservation of the title.<br></br>
+9.8. The costs incurred on account of the enforcement of the Contractor's title shall be regarded<br></br>
+as the Buyer's costs.<br></br><br></br>
+10. Concluding provisions<br></br>
+10.1. These trading terms and conditions were executed in Czech, Slovak, English and German. Should<br></br>
+any differences arise among individual versions, the Czech language version shall prevail.<br></br>
+10.2. The relations between the parties shall be governed by the Czech legal order. Any legal relations not<br></br>
+regulated in the contract or these terms and conditions expressly shall be governed by Act 513/1991<br></br>
+Coll., Commercial Code. In cases of contracts with an international element, the United Nations<br></br>
+Convention on Contracts for the International Purchase of Goods (Vienna, 1980) shall be applied,<br></br>
+which is part of the Czech legal order; the Convention according to the clause in front of the<br></br>
+semicolon shall be applied even if the Buyer does not have a registered office in the country which is<br></br>
+a party to the Convention.<br></br>
+10.3. If any of the provisions of these trading terms and conditions is or becomes ineffective, other<br></br>
+provisions of these terms and conditions shall remain in effect. The parties undertake to supersede<br></br>
+the ineffective provision of these terms and conditions by another effective provision which shall<br></br>
+correspond best to the content and sense of the original ineffective provision. Any invalidity of<br></br>
+individual provisions of the General Trading Terms and Conditions shall not affect the validity of<br></br>
+other provisions.<br></br>
+10.4. If the content of a concluded individual contract is inconsistent with the provisions of these General<br></br>
+Trading Terms and Conditions, the individual contract shall prevail.<br></br>
+10.5. All disputes in relation to the contract concluded between the parties or these General Trading Terms<br></br>
+and Conditions shall be settled definitely in proceedings before the Arbitration Court at the<br></br>
+Economic Chamber of the Czech Republic and Agrarian Chamber of the Czech Republic according<br></br>
+to its Rules and Regulations by a tribunal consisting of three arbitrators. By the provision of the<br></br>
+previous sentence, the right of the parties to sue for recovery of their claims by a petition filed at the<br></br>
+Municipal Court in Brno or Regional Court in Brno, depending on the subject-matter jurisdiction of<br></br>
+the court with respect to a specific dispute, is not to be excluded.<br></br>
+10.6. The Buyer expresses unreserved consent to these General Trading Terms and Conditions and<br></br>
+confirms the fact by its signature. <br></br><br></br><br></br>
+
+</div>
+
+</>
+)
+}
+}
+
+export default Tradcon;
