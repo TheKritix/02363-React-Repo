8bde9bf9c50d60fb186fcbe5d49b3dea664764fd
Camilla <s205360@student.dtu.dk>
2022-04-27 13:28:29 +0200

    logout function modified

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,6 +36,7 @@ export default function Login({ setToken, setUserId }) {
       console.log(token);
       setToken(token);
       setUserId(token);
+      
       () => <Topbar onClick={handleProfileName()}/>
     } 
 
--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -9,120 +9,113 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import { Favorite } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { IconButton } from "@mui/material";
-import PropTypes from "prop-types";
 
 
 export const Topbar = () => {
+  const [loginDest, setLoginDest] = useState("/loginpage");
+  const [login, setLogin] = useState([
+    {
+      Type: "login",
+      URL: "./loginpage",
+    },
+  ]);
 
-    const [login, setLogin] = useState([
-      {
-       Type: "login",
-       URL: "./loginpage",
-     }]
-    );
-
-        const checkUser = () => {
-      if (sessionStorage.getItem('token')) { 
+  const checkUser = () => {
+    if (sessionStorage.getItem("token")) {
       handleProfileName();
-      }
-     else {
-     handleLogin();
-     
-        
-      }
+    } else {
+      handleLogin();
     }
-    
-    useEffect(() => {
-      checkUser();
-    }, []);
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const logout = (setToken) => {
+    setToken(sessionStorage.clear());
+    console.log("logged out");
+  };
 
 
-    const logout = (setToken) => {
-      setToken(sessionStorage.clear());
-      console.log("logged out");
-    };
+  const handleLogin = () => {
+    setLogin({ Type: "Login" });
+    setLoginDest("/loginpage");
+  };
 
-   const handleLogin = () => {
-      setLogin({Type:'Login', URL: './'});
-    }
- 
-    const handleProfileName = () => {
-      console.log("horse shit")
-      setLogin({Type:'Profile'})
-    }
+  const handleProfileName = () => {
+    console.log("horse shit");
+    setLogin({ Type: "Profile" });
+    setLoginDest("/profilepage");
+  };
 
-    
-
-    return (
-      <div className="Topbar">
-        <div className="divLogo">
-          <Link id="logoLink" to="/">
-            {" "}
-            <h1 className="logo">
-              <img src={logo} alt="logoImage" id="logoImage" />
-              <div id="stoodleText">
-                St<span className="oo">oo</span>dle
-              </div>
-            </h1>
-          </Link>
-        </div>
-        <div className="search-div">
-          <TextField
-            alignItems
-            className="search"
+  return (
+    <div className="Topbar">
+      <div className="divLogo">
+        <Link id="logoLink" to="/">
+          {" "}
+          <h1 className="logo">
+            <img src={logo} alt="logoImage" id="logoImage" />
+            <div id="stoodleText">
+              St<span className="oo">oo</span>dle
+            </div>
+          </h1>
+        </Link>
+      </div>
+      <div className="search-div">
+        <TextField
+          alignItems
+          className="search"
+          size="small"
+          placeholder="Search..."
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon id="searchIcon" />
+              </InputAdornment>
+            ),
+          }}
+        ></TextField>
+      </div>
+      <div className="options">
+        <div className="topbar-menu">
+          <IconButton
             size="small"
-            placeholder="Search..."
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon id="searchIcon" />
-                </InputAdornment>
-              ),
-            }}
-          ></TextField>
-        </div>
-        <div className="options">
-          <div className="topbar-menu">
-            <IconButton
-              size="small"
-              style={{ width: 140 }}
-              className="topbar-links"
-              onClick=""
-            >
-              {" "}
-              <NotificationsNoneIcon className="icon"></NotificationsNoneIcon>
-              <text>Notifications</text>
-            </IconButton>
-            <IconButton
-              size="small"
-              style={{ width: 80 }}
-              className="topbar-links"
-              component={Link}
-              to="/favoritpage"
-            >
-              <Favorite id="favorit" className="icon"></Favorite> Favorites
-            </IconButton>
-            <IconButton
-              size="small"
-              style={{ width: 80 }}
-              className="topbar-links"
-              component={Link}
-              to="/loginpage"
-              onClick={() => checkUser()}
-            >
-              <AccountCircleIcon className="icon" ></AccountCircleIcon>
-              {login.Type}
-            </IconButton>
-          </div>
+            style={{ width: 140 }}
+            className="topbar-links"
+            onClick=""
+          >
+            {" "}
+            <NotificationsNoneIcon className="icon"></NotificationsNoneIcon>
+            <text>Notifications</text>
+          </IconButton>
+          <IconButton
+            size="small"
+            style={{ width: 80 }}
+            className="topbar-links"
+            component={Link}
+            to="/favoritpage"
+          >
+            <Favorite id="favorit" className="icon"></Favorite> Favorites
+          </IconButton>
+          <IconButton
+            size="small"
+            style={{ width: 80 }}
+            className="topbar-links"
+            component={Link}
+            to={loginDest}
+            onClick={() => checkUser()}
+          >
+            <AccountCircleIcon className="icon"></AccountCircleIcon>
+            {login.Type}
+          </IconButton>
         </div>
       </div>
-    );
-  }
-
+    </div>
+  );
+};
 
 export default Topbar;
-
-
 
 /*return (
         <IconButton Size="small" style={{width: 40}}>
@@ -130,6 +123,8 @@ export default Topbar;
           <AccountCircleIcon
             className="icon"
             onclick={logout}
+            component={Link}
+            to={"/homepage"}
           ></AccountCircleIcon>
           Logout
         </IconButton>
