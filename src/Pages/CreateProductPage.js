45d8afd6d06aa85b5440ed53f655aa717a958697
Kristoffer <kristoffer@kritix.dk>
2022-05-01 20:46:18 +0200

    Fixed Register'

--- src/Pages/CreateProductPage.js
+++ src/Pages/CreateProductPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import CreateProducts from "../components/CreateProducts/CreateProducts";
 import Login from "../components/Login/Login";
-import useUserId from '../components/Login/useUserId';
-import useToken from '../components/Login/useToken';
+import useUserId from "../components/Login/useUserId";
+import useToken from "../components/Login/useToken";
 
 function CreateProductPage() {
-
-    const {token, setToken} = useToken();
-    const {userId, setUserId} = useUserId();
-  
+  const { token, setToken } = useToken();
+  const { userId, setUserId } = useUserId();
 
   if (window.sessionStorage.getItem("token")) {
     return (
@@ -19,7 +17,7 @@ function CreateProductPage() {
   } else {
     return (
       <>
-        <Login setToken={setToken} setUserId={setUserId}/> 
+        <Login setToken={setToken} setUserId={setUserId} />
       </>
     );
   }
