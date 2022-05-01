c1638083a7f5f8c6b2d742d9db2068548da9bca1
Kristoffer <kristoffer@kritix.dk>
2022-05-01 21:45:39 +0200

    Fixed remaining stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -22,29 +22,56 @@ class Footer extends Component {
                   </Link>
                 </li>
                 <li className="list-item">
-                <Link className="tradecon" to="/tradecondition">
+                  <Link className="tradecon" to="/tradecondition">
                     Trade Condition
                   </Link>
                 </li>
-                <li className="list-item">PLACEHOLDER</li>
               </ul>
             </article>
             <article className="footer-article">
               <h2 className="footer-title">MY ACCOUNT</h2>
               <ul className="footer-ul">
-                <li className="list-item">PLACEHOLDER</li>
-                <li className="list-item">PLACEHOLDER</li>
-                <li className="list-item">PLACEHOLDER</li>
-                <li className="list-item">PLACEHOLDER</li>
+                <li className="list-item">
+                  {" "}
+                  <Link className="profile" to="/profilepage">
+                    Profile
+                  </Link>
+                </li>
+                <li className="list-item">
+                  {" "}
+                  <Link className="favorites" to="/">
+                    Favorites
+                  </Link>
+                </li>
+                <li className="list-item">
+                  {" "}
+                  <Link className="notifications" to="/">
+                    Notifications
+                  </Link>
+                </li>
               </ul>
             </article>
             <article className="footer-article">
               <h2 className="footer-title">MICHELLINAOUS</h2>
               <ul className="footer-ul">
-                <li className="list-item">PLACEHOLDER</li>
-                <li className="list-item">PLACEHOLDER</li>
-                <li className="list-item">PLACEHOLDER</li>
-                <li className="list-item">PLACEHOLDER</li>
+                <li className="list-item">
+                  {" "}
+                  <Link className="buy" to="/buy">
+                    Buy
+                  </Link>
+                </li>
+                <li className="list-item">
+                  {" "}
+                  <Link className="sell" to="/sell">
+                    Sell
+                  </Link>
+                </li>
+                <li className="list-item">
+                  {" "}
+                  <Link className="homepage" to="/">
+                    Homepage
+                  </Link>
+                </li>
               </ul>
             </article>
           </section>
