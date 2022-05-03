01324a7f92fd2ab16540c4878b82cf8408ed74f6
Kristoffer <kristoffer@kritix.dk>
2022-05-03 20:02:16 +0200

    Added comments

--- src/components/Login/useToken.js
+++ src/components/Login/useToken.js
@@ -1,6 +1,8 @@
 import { func } from "prop-types";
 import { useState } from "react";
 
+//Source: https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications?fbclid=IwAR2y4l2yBvpOauFUEiDdHZho_a-fYX0bwlhNwei_j4iIm-fq3EVRsf3vJ5s
+
 export default function useToken() {
     const getToken = () => {
         const tokenString = sessionStorage.getItem("token");
